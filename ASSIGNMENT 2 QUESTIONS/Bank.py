2a48c2b31954d59cfa71922597f3077fe5a47bd3
SammyM001 <110180961+SammyM001@users.noreply.github.com>
2022-08-25 23:53:36 +0300

    Add files via upload

---
+++ ASSIGNMENT 2 QUESTIONS/Bank.py
@@ -0,0 +1,12 @@
+greetings=input("Kindly enter a greeting: ")
+is_hello= True
+is_h= True
+if is_hello:
+    print("$0")
+elif is_h and not(is_hello):
+    print("$20")
+else:
+    print("$100")
+
+
+
